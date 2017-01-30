946f58141d34c3d8c24bec3181023f5d5abb6a6c
Denver Green <greewden@gmail.com>
2017-01-29 23:12:38 -0500

    Solution to Fradulent Notification challenge using counting sort.

--- src/test/java/com/denver/green/FraudNotificationTest.java
+++ src/test/java/com/denver/green/FraudNotificationTest.java
@@ -1,6 +1,12 @@
 package com.denver.green;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,13 +14,30 @@ import static org.junit.Assert.assertEquals;
  * Created by degreen on 2017-01-27.
  */
 public class FraudNotificationTest {
+
+    private Scanner in;
+
+    @Before
+    public void setUp() throws Exception {
+        // Locate input file describing graph and create an input stream that
+        // will read from it.
+        String workingDir = System.getProperty("user.dir");
+        System.out.println("Current working directory: " + workingDir);
+        FileInputStream fis = new FileInputStream(
+                "src" + File.separator +"test" + File.separator + "resources" + File.separator + "median.txt");
+        in = new Scanner(new BufferedInputStream(fis));
+    }
+
     @Test
     public void countNotifications() throws Exception {
-        int[] expenditures = {1, 2, 3, 4, 4};
-        int window = 4;
-
-        FraudNotification fn = new FraudNotification(window, expenditures);
-        assertEquals(0, fn.countNotifications());
+        int timePeriod = in.nextInt();
+        int windowSize = in.nextInt();
+        int[] expenditures = new int[timePeriod];
+        for (int i = 0; i < timePeriod; i++) {
+            expenditures[i] = in.nextInt();
+        }
+        FraudNotification fn = new FraudNotification(windowSize, expenditures);
+        assertEquals(926, fn.countNotifications());
     }
 
 }
